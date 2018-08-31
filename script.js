34d4b25a2accc571f0eeed8f4207502271cc6d29
Ritik Srivastava <ritik.space@gmail.com>
2018-08-31 17:04:38 +0530

    Achievements Galary

--- script.js
+++ script.js
@@ -2,20 +2,11 @@ var navMenu=document.querySelector('.navbar-toggler');
 var myFullpage = new fullpage('#fullpage', {
 	anchors: ['Home', 'Education', 'Expertise', 'Achievement', 'Endeavor', 'Organisation', 'Connect'],
 	sectionsColor: ['#FFF', '#FFF','#FFF', '#FFF'],
-	menu: '#navmenu',
-
 	direction: 'vertical',
-	scrollingSpeed: 450,
-
-	navigation: {
-		'position': 'right',
-		'textColor': '#111',
-		'bulletsColor': '#111',
-		'position': 'right',
-		'tooltips': ['Home', 'Education', 'Expertise', 'Achievement', 'Endeavor', 'Organisation', 'Connect']
-	}
+	scrollingSpeed: 500,
 });
 
 navMenu.addEventListener('click',function(){
 	navMenu.classList.toggle('open');
 });
+
