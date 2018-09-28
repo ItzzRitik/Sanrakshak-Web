96f7538ddb67a444752b71382323e85a5c49a870
Ritik Srivastava <ritik.space@gmail.com>
2018-09-28 11:55:02 +0530

    Profile

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var menuLink=document.querySelector('.menu-link');
 var menu=document.querySelector('.menu');
 var menuOverlay=document.querySelector('.menu-overlay');
+var profile_info=document.querySelector('.more_info');
+var profile_card=document.querySelectorAll('.profile_card');
+
 var myFullpage = new fullpage('#fullpage', {
 	anchors: ['Home', 'Education', 'Expertise', 'Achievement', 'Endeavor', 'Organisation', 'Connect'],
 	sectionsColor: ['#FFF', '#FFF','#FFF', '#FFF'],
@@ -12,4 +15,8 @@ menuLink.addEventListener('click',function(e){
 	e.preventDefault();
 	menu.classList.toggle('open');
 	menuOverlay.classList.toggle('open');
+});
+profile_info.addEventListener('click',function(e){
+	profile_info
+	profile_card.classList.toggle('profile_card_flip');
 });
